7d00336eaaec43d848bd215026c18c7f866a3008
harshit <harshit.tiwari2020@vitbhopal.ac.in>
2022-10-19 16:52:13 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+$.ajax({
+    type: "get",
+    url: "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian",
+    
+    success: function (response) {
+        for(var i=0; i<response.meals.length; i++){
+            var newitems = `<div class="col-md-3 m-2 text-center shadow-lg p-3 mb-5 bg-white rounded">
+                <p>${response.meals[i].strMeal}</p>
+                <img class="img-fluid" src="${response.meals[i].strMealThumb}"/>
+                <p>${response.meals[i].idMeal}</p>
+            </div>`
+
+            $('#myitems').append(newitems);
+        }
+    }
+});
